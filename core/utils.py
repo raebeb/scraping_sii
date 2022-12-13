64a31bdb6d4474ad97e3757fa628c9e91675b463
Fran Osores <fosores@linets.cl>
2022-12-12 21:50:59 -0300

    UPDATE login way to a class

--- core/utils.py
+++ core/utils.py
@@ -3,56 +3,51 @@ import requests
 
 from .rut import rut_to_dict
 
-def scrap_html(url):
-    request = requests.get(url)
-    return BeautifulSoup(request.text, 'html.parser')
+class SiiUser:
+    def __init__(self, rut, password):
+        self.rut = rut
+        self.password = password
+        self.session = self.login(self.rut, self.password)
+        self.cookies = requests.utils.dict_from_cookiejar(self.session.cookies)
+        self.token = self.cookies['TOKEN']
 
 
-def create_data_for_login(rut, password):
-    data = rut_to_dict(rut)
+    def create_data_for_login(self, rut, password):
+        data = rut_to_dict(rut)
 
-    return {
-        'rut': data["rut"],
-        'dv': data["dv"],
-        'referencia': 'https://misiir.sii.cl/cgi_misii/siihome.cgi',
-        '411': '',
-        'rutcntr': data["rutcntr"],
-        'clave': password,
-    }
-    
-def login_to_sii(rut, password):
-    session_requests = requests.Session()
+        return {
+            'rut': data["rut"],
+            'dv': data["dv"],
+            'referencia': 'https://misiir.sii.cl/cgi_misii/siihome.cgi',
+            '411': '',
+            'rutcntr': data["rutcntr"],
+            'clave': password,
+        }
 
-    headers = {
-        'Origin': 'https://zeusr.sii.cl',
-        'Referer': 'https://zeusr.sii.cl//AUT2000/InicioAutenticacion/IngresoRutClave.html?https://misiir.sii.cl/cgi_misii/siihome.cgi',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
-        'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Linux"',
-    }
+    def login(self, rut, password):
+        session_requests = requests.Session()
 
-    data = create_data_for_login(rut, password)
-    response = session_requests.post('https://zeusr.sii.cl/cgi_AUT2000/CAutInicio.cgi', headers, data)
-    
-    soup = BeautifulSoup(response.text, 'html.parser')
-    if soup.find(id="auth_bottom"):
-        print("Login unsuccessful")
+        data = self.create_data_for_login(rut, password)
+        response = session_requests.post('https://zeusr.sii.cl/cgi_AUT2000/CAutInicio.cgi', data=data)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        if soup.find(id="auth_bottom"):
+            print("Login unsuccessful")
+            return False
+        print("Login successful")
+
+        return session_requests
+
+    def connect_to_page(self, url: str, session: requests.Session):
+        if session:
+            return session.get(url)
         return False
-    print("Login successful")
-    
-    return session_requests
-
-def connect_to_page(url, session):
-    return session.get(url)
 
 
 # python manage.py shell
 # from core.utils import *
 # response_login = login_to_sii('782413708', 'union2')
-# Login successful
-# resp = connect_to_page('https://www.sii.cl/servicios_online/', response_login)
+# resp = connect_to_page('https://www4.sii.cl/consdcvinternetui/', response_login)
 # soup = BeautifulSoup(resp.text, 'html.parser')
 # title = soup.find('h1')
 # title
