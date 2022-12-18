28d95d17e4ea7b51be1f06bfcfe3ade5ae280590
Fran <raebeb@gmail.com>
2022-12-18 00:34:37 -0300

    ADD endpoint to return personal data requested

--- api/views.py
+++ api/views.py
@@ -1,16 +1,35 @@
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from .serializers import ClientSerializer
+from rest_framework.decorators import action
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-class ClientViewSet(viewsets.ViewSet):
+from core.sii_user import SiiUser
+from core.utils import get_company_data
+
+class SIIDataViewSet(viewsets.ViewSet):
+
+    def get_queryset(self):
+        queryset = ''
+        return queryset
+
     def list(self, request):
         serializer = ClientSerializer()
         return Response(serializer.data)
 
-    def retrieve(self, request):
-        client = '' #TODO get client from scraped data
-        #TODO: 1 -> Create the client, 2 -> with the client created get the data from sii_scraper
-        serializer = ClientSerializer(client)
-        return Response(serializer.data)
+    @action(detail=False, methods=['get'])
+    def personal_and_tax_data(self, request):
+        query_params = request.query_params
+        rut = query_params.get('rut')
+        password = query_params.get('password')
+        sii_user = SiiUser(rut, password)
+
+        if sii_user:
+            company_data = get_company_data(sii_user)
+            return Response(company_data)
+        else:
+            return Response("Invalid credentials")
+
+
+
