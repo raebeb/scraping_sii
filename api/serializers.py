d9f920a9fd074d22863748bb7606f6647e5f8f04
Fran <raebeb@gmail.com>
2022-12-18 01:03:27 -0300

    UPDATE serializers to match data format

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,40 @@
 from rest_framework import serializers
 
-class ClientSerializer(serializers.Serializer):
-    company_name = serializers.CharField()
+
+class LegalRepresentativeSerializer(serializers.Serializer):
+    name = serializers.CharField()
     rut = serializers.CharField()
-    address = serializers.CharField()
-    date_of_construction = serializers.CharField()
-    activity_start_date = serializers.CharField()
-    current_legal_representatives = serializers.CharField()
-    current_partners = serializers.CharField()
-    current_economic_activities = serializers.CharField()
-    characteristics = serializers.CharField()
-    authorized_tax_documents = serializers.CharField()
+    from_date = serializers.CharField()
+
+
+class CurrentPartnerSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    rut = serializers.CharField()
+    aware_capital = serializers.IntegerField()
+    capital_to_find_out = serializers.IntegerField()
+    capital_percentage = serializers.FloatField()
+    percentage_utilities = serializers.FloatField()
+    membership_from = serializers.CharField()
+
+
+class ActiveEconomicActivitySerializer(serializers.Serializer):
+    code = serializers.CharField()
+    name = serializers.CharField()
+    category = serializers.IntegerField()
+    taxable = serializers.BooleanField()
+    start_at = serializers.CharField()
+
+
+class CharacteristicSerializer(serializers.Serializer):
+    start_at = serializers.CharField()
+    description = serializers.CharField()
+
+
+class CompanySerializer(serializers.Serializer):
+    legal_representative = LegalRepresentativeSerializer(many=True)
+    constitution_date = serializers.CharField()
+    start_of_activities = serializers.CharField()
+    full_address = serializers.CharField()
+    current_partners = CurrentPartnerSerializer(many=True)
+    active_economic_activities = ActiveEconomicActivitySerializer(many=True)
+    characteristics = CharacteristicSerializer(many=True)
